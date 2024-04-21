0d8564f5f7d5871d9efe865b883950cea9507c23
Harshal Mali <20clashid1@gmail.com>
2024-04-21 18:15:54 +0530

    more changes done

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken"); // generate the token and vewrify the token
-const multer = require("multer");
+
 const path = require("path");
 const cors = require("cors");
 const { default: all_product } = require("./all_product");
@@ -19,26 +19,6 @@ mongoose.connect(
 //API Creation
 app.get("/", (req, res) => {
 	res.send("Express App is Running");
-});
-
-// Image Storage Engine
-
-const storage = multer.diskStorage({
-	destination: "./upload/images",
-	filename: (req, file, cb) => {
-		return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
-	},
-});
-const upload = multer({ storage: storage });
-
-//creating Upload Endpoint images
-app.use("/images", express.static("upload/images"));
-
-app.post("/upload", upload.single("product"), (req, res) => {
-	res.json({
-		success: 1,
-		image_url: `http://localhost:${port}/images/${req.file.filename}`,
-	});
 });
 
 //Schema for Creating Products
